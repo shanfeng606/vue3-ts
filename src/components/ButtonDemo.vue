a9cdde50e1896bc4a17ca2e67d3c38f4938348d7
SYF <1275260388@qq.com>
2020-12-25 10:52:40 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
-        ButtonDemo
-    </div>
+  <Button>你好</Button>
 </template>
+<script lang="ts">
+import Button from "../lib/Button.vue"
+export default {
+    components:{Button}
+}
+</script>
