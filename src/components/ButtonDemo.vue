9301e171fa7ea44dcf4bee4ed03292a4ce13e144
SYF <1275260388@qq.com>
2020-12-25 14:13:38 +0800

    Button的基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,9 +1,24 @@
 <template>
-  <Button>你好</Button>
+  <div>
+    <div>Button 示例</div>
+    <h1>示例1</h1>
+    <div>
+      <Button>你好</Button>
+      <Button theme="button">你好</Button>
+      <Button theme="link">你好</Button>
+      <Button theme="text">你好</Button>
+    </div>
+  </div>
 </template>
 <script lang="ts">
-import Button from "../lib/Button.vue"
+import Button from "../lib/Button.vue";
 export default {
-    components:{Button}
-}
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  }
+};
 </script>
